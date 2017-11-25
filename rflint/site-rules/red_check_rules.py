c58b77ad9bfc1a63c93e9635487ee1933ba47402
hpschry <hpschry@googlemail.com>
2017-11-25 14:16:34 +0100

    detect-redundancy: revised for keyword and test bodies

--- rflint/site-rules/red_check_rules.py
+++ rflint/site-rules/red_check_rules.py
@@ -3,6 +3,7 @@ from rflint.parser.rfkeyword import Keyword
 from rflint.parser.testcase import Testcase
 
 from numpy import finfo, argwhere, logical_and, nonzero, fill_diagonal, sort
+import numpy
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import re
@@ -10,39 +11,18 @@ import re
 class PairwiseSimilarity (object):
     
     def find (self, cos_lower, cos_upper, doc_list):
-        print (str(doc_list))
+        """Return the coordinates of similar documents
+        """
         tfidf = TfidfVectorizer().fit_transform(doc_list)
-        print ("tfidf:\n" + str (tfidf.A))
         pwsim_array = (tfidf * tfidf.T).A
-        count = 0
-        fill_diagonal (pwsim_array, 0)
-        print ("pwsim:\n" + str (pwsim_array))
+        pwsim_array = numpy.triu (pwsim_array, 1)
         cos_lower -= finfo("float64").eps
         cos_upper += finfo("float64").eps
         similars = argwhere (
             logical_and (pwsim_array >= cos_lower, cos_upper >= pwsim_array))
-        print ("unsorted:\n" + str (similars))
-        sorted_similars = similars[similars[:,1].argsort()]
-        print ("sorted:\n" + str (sorted_similars))
+        sorted_similars = similars[similars[:,0].argsort()]
+        print ("shape: " + str (numpy.shape (sorted_similars)))
         return sorted_similars
-    """
-        print ("argwhere:\n" + str ()
-        for i, r in enumerate (pwsim.A [:-1]):
-            for j, cosphi in enumerate (r [i+1:], start=i+1):
-                # print (i, j, cosphi)
-                if (cosphi >= self.cosphi_low) and (cosphi <= self.cosphi_high + finfo("float64").eps):
-                    count += 1
-                    src_kw = StoreKW.kw_list [i]
-                    trg_kw = StoreKW.kw_list [j]
-                    msg = "High keyword body similarity ({}) - redundant?\n  {} ({}, {})\n  {} ({}, {})".format (
-                        cosphi, src_kw.name, src_kw.path, src_kw.linenumber, trg_kw.name, trg_kw.path, trg_kw.linenumber)
-                    self.report (src_kw, msg, 0)
-        msg = "W: High keyword body similarity ({} <= cosphi <= {}) count is {}".format (
-            self.cosphi_low, self.cosphi_high, count)
-        print (msg)
-    """
-
-
     
 
 class StoreTest (TestRule):
@@ -65,6 +45,13 @@ class StoreKW (KeywordRule):
 
     def apply (self, keyword):
         self.kw_list.append (keyword)
+
+
+def _format_msg (src_doc, trg_doc):
+    msg = ("High keyword body similarity - redundant?\n {} ({}, {})\n {} ({}, {})".
+           format (src_doc.name, src_doc.path,
+            src_doc.linenumber, trg_doc.name, trg_doc.path, trg_doc.linenumber))
+    return msg
 
 
 class KWRedundantName (PostRule):
@@ -108,14 +95,16 @@ class KWRedundantName (PostRule):
         
 
 class KWRedundantBody (PostRule):
-    '''Determine redundant resp. similar keyword implementations.
+    '''Determine redundant resp. similar keyword and test implementations.
     
-    Based on the assumption, that very similar keyword implementations
-    might indicate duplicate or similar implementations.
+    Based on the assumption, that very similar keyword and test
+    implementations might indicate duplicate or similar
+    implementations (copies).
 
     Based on the approach described in:
-    http://stackoverflow.com/questions/8897593/similarity-between-two-text-documents'''
+    http://stackoverflow.com/questions/8897593/similarity-between-two-text-documents
 
+    '''
     cosphi_low = 0.9
     cosphi_high = 1.0
 
@@ -124,10 +113,18 @@ class KWRedundantBody (PostRule):
         self.cosphi_high = float (high)
     
     def apply (self, dummy):
-
-        kw_bodies = ["\n".join ([r.raw_text for r in kw.rows]) for kw in StoreKW.kw_list]
-        similars = PairwiseSimilarity().find(self.cosphi_low, self.cosphi_high, kw_bodies)
-        print (similars)
+        doc_list = StoreKW.kw_list + StoreTest.test_list
+        kw_bodies = [" ".join ([r.raw_text for r in kw.rows]) for kw in doc_list]
+        print ("bodies:" + str (kw_bodies))
+        pairs = PairwiseSimilarity ().find (self.cosphi_low, self.cosphi_high, kw_bodies)
+        for pair in pairs:
+            msg = _format_msg (doc_list[pair[0]], doc_list[pair[1]])
+            self.report (doc_list[pair[0]], msg, 0)
+        msg = "W: Pairwise similarities ({} <= cosphi <= {}) count is {}".format (
+            self.cosphi_low, self.cosphi_high, numpy.shape(pairs)[0])
+        print (msg)
+        
+        
 
 
     """
