a3df471eadbcd72a762a92134c9b552df8867a75
hpschry <hpschry@googlemail.com>
2017-12-01 22:50:27 +0100

    detect-redundancy: prototype refined

--- rflint/site-rules/red_check_rules.py
+++ rflint/site-rules/red_check_rules.py
@@ -11,18 +11,21 @@ import re
 class PairwiseSimilarity (object):
     
     def find (self, cos_lower, cos_upper, doc_list):
-        """Return the coordinates of similar documents
+        """Return the indices of similar documents
         """
-        tfidf = TfidfVectorizer().fit_transform(doc_list)
-        pwsim_array = (tfidf * tfidf.T).A
-        pwsim_array = numpy.triu (pwsim_array, 1)
-        cos_lower -= finfo("float64").eps
-        cos_upper += finfo("float64").eps
-        similars = argwhere (
-            logical_and (pwsim_array >= cos_lower, cos_upper >= pwsim_array))
-        sorted_similars = similars[similars[:,0].argsort()]
-        print ("shape: " + str (numpy.shape (sorted_similars)))
-        return sorted_similars
+        try:
+            tfidf = TfidfVectorizer().fit_transform(doc_list)
+            pwsim_array = (tfidf * tfidf.T).A
+            pwsim_array = numpy.triu (pwsim_array, 1)
+            cos_lower -= finfo("float64").eps
+            cos_upper += finfo("float64").eps
+            similars = argwhere (
+                logical_and (pwsim_array >= cos_lower, cos_upper >= pwsim_array))
+            sorted_similars = similars[similars[:,0].argsort()]
+            return sorted_similars
+        except ValueError as e:
+            print ("Error in processing document collection: " + str(e))
+            return numpy.array([])
     
 
 class StoreTest (TestRule):
@@ -39,8 +42,8 @@ class StoreKW (KeywordRule):
 
     Put the keyword objects in a list as they are traversed.
     This is for later processing (finding similar keyword 
-    names and bodies.'''
-
+    names and bodies.
+    '''
     kw_list = []
 
     def apply (self, keyword):
@@ -77,20 +80,12 @@ class KWRedundantName (PostRule):
     
     def apply (self, dummy):
         kw_names = [k.name for k in StoreKW.kw_list]
-        tfidf = TfidfVectorizer().fit_transform(kw_names)
-        pwsim = tfidf * tfidf.T
-        count = 0
-        for i, r in enumerate (pwsim.A [:-1]):
-            for j, cosphi in enumerate (r [i+1:], start=i+1):
-                if (cosphi >= self.cosphi_low) and (cosphi <= self.cosphi_high):
-                    count = count + 1
-                    src_kw = StoreKW.kw_list [i]
-                    trg_kw = StoreKW.kw_list [j]
-                    msg = "High keyword name similarity ({}) - redundant?\n  {} ({}, {})\n  {} ({}, {})".format (
-                        cosphi, src_kw.name, src_kw.path, src_kw.linenumber, trg_kw.name, trg_kw.path, trg_kw.linenumber)
-                    self.report (src_kw, msg, 0)
+        pairs = PairwiseSimilarity().find(self.cosphi_low, self.cosphi_high, kw_names)
+        for pair in pairs:
+            msg = _format_msg (StoreKW.kw_list[pair[0]], StoreKW.kw_list[pair[1]])
+            self.report (StoreKW.kw_list[pair[0]], msg, 0)
         msg = "W: High keyword name similarity ({} <= cosphi <= {}) count is {}".format (
-            self.cosphi_low, self.cosphi_high, count)
+            self.cosphi_low, self.cosphi_high, numpy.shape(pairs)[0])
         print (msg)
         
 
@@ -114,8 +109,7 @@ class KWRedundantBody (PostRule):
     
     def apply (self, dummy):
         doc_list = StoreKW.kw_list + StoreTest.test_list
-        kw_bodies = [" ".join ([r.raw_text for r in kw.rows]) for kw in doc_list]
-        print ("bodies:" + str (kw_bodies))
+        kw_bodies = [" ".join ([r.raw_text for r in kw.rows]) for kw in doc_list if kw.steps]
         pairs = PairwiseSimilarity ().find (self.cosphi_low, self.cosphi_high, kw_bodies)
         for pair in pairs:
             msg = _format_msg (doc_list[pair[0]], doc_list[pair[1]])
@@ -124,32 +118,3 @@ class KWRedundantBody (PostRule):
             self.cosphi_low, self.cosphi_high, numpy.shape(pairs)[0])
         print (msg)
         
-        
-
-
-    """
-        print (str(kw_bodies))
-        tfidf = TfidfVectorizer().fit_transform(kw_bodies)
-        print ("tfidf:\n" + str (tfidf.A))
-        pwsim = tfidf * tfidf.T
-        print ("pwsim:\n" + str (pwsim.A))
-        count = 0
-        arr = pwsim.A
-        fill_diagonal (arr, 0)
-        print ("argwhere:\n" + str (argwhere (logical_and (arr >= self.cosphi_low, self.cosphi_high + finfo("float64").eps >= arr))))
-        for i, r in enumerate (pwsim.A [:-1]):
-            for j, cosphi in enumerate (r [i+1:], start=i+1):
-                # print (i, j, cosphi)
-                if (cosphi >= self.cosphi_low) and (cosphi <= self.cosphi_high + finfo("float64").eps):
-                    count += 1
-                    src_kw = StoreKW.kw_list [i]
-                    trg_kw = StoreKW.kw_list [j]
-                    msg = "High keyword body similarity ({}) - redundant?\n  {} ({}, {})\n  {} ({}, {})".format (
-                        cosphi, src_kw.name, src_kw.path, src_kw.linenumber, trg_kw.name, trg_kw.path, trg_kw.linenumber)
-                    self.report (src_kw, msg, 0)
-        msg = "W: High keyword body similarity ({} <= cosphi <= {}) count is {}".format (
-            self.cosphi_low, self.cosphi_high, count)
-        print (msg)
-    """
-
-    
